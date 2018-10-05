a026e867949afb373ea27ffa9d458a704aabf346
Danakt Frost <danakt_frost@mail.ru>
2018-10-05 19:03:51 +0300

    Init configs

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import { Configuration }      from 'webpack'
+import * as path              from 'path'
+import * as HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const config: Configuration = {
+  devtool: 'source-map',
+  entry:   './src/index.ts',
+  module:  {
+    rules: [
+      {
+        test:    /\.tsx?$/,
+        use:     'awesome-typescript-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.mdl$/,
+        use:  'file-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path:     path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'template.html')
+    })
+  ]
+}
+
+export default config
