748ec2969183c1902aabcd6022bba2282231fcbb
Danakt Frost <danakt_frost@mail.ru>
2018-10-23 01:11:53 +0300

    Update rendering functions

--- lib/modelRenderer.ts
+++ lib/modelRenderer.ts
@@ -4,16 +4,6 @@ import * as constants            from '../const/constants'
 import * as structs              from '../const/structs'
 
 /**
- * Type of description of face vertices
- */
-type VertexDesc = {
-  origin: THREE.Vector3
-  light: number
-  uv: THREE.Vector2 // Coordinates of UV-mapping
-  index: number
-}
-
-/**
  * Build image data from buffer
  * @param buffer The model buffer
  * @param texture Texture description
@@ -82,9 +72,9 @@ export const createSkeleton = (bonesData: structs.Bone[]): THREE.Skeleton => {
     bone.position.y = bonesData[i].value[1]
     bone.position.z = bonesData[i].value[2]
 
-    // bone.scale.x = bonesData[i].scale[0]
-    // bone.scale.y = bonesData[i].scale[1]
-    // bone.scale.z = bonesData[i].scale[2]
+    // bone.scale.x = bonesData[i].scale[3]
+    // bone.scale.y = bonesData[i].scale[4]
+    // bone.scale.z = bonesData[i].scale[5]
 
     if (bonesData[i].parent > -1) {
       bones[bonesData[i].parent].add(bone)
@@ -97,47 +87,89 @@ export const createSkeleton = (bonesData: structs.Bone[]): THREE.Skeleton => {
 }
 
 /**
- * Returns face vertices of the mesh
+ * Returns type of a series of connected triangles
  */
-export const getFaceVertices = (trisBuffer: Int16Array, vertices: Float32Array, texture: ImageData): VertexDesc[] => {
-  // List of vertices of faces
-  const faceVertices: VertexDesc[] = []
+export const getTrianglesType = (trianglesSeriesHead: number) =>
+  trianglesSeriesHead < 0 ? constants.TRIANGLE_FAN : constants.TRIANGLE_STRIP
 
-  // Position in mesh triangles buffer
-  let i = 0
+/**
+ * Counts vertices for building geometry buffer
+ * @param trianglesBuffer Triangles buffer
+ * @return Number of vertices
+ */
+export const countVertices = (trianglesBuffer: Int16Array): number => {
+  // Count of vertices
+  let vertCount = 0
 
-  while (trisBuffer[i]) {
-    const isTriangleStrip: boolean = trisBuffer[i] >= 0
-    const isTriangleFan: boolean = trisBuffer[i] < 0
+  // Current position in buffer
+  let p = 0
 
+  // Processing triangle series
+  while (trianglesBuffer[p]) {
+    // Number of following vertices
+    const verticesNum = Math.abs(trianglesBuffer[p])
+
+    // This position is no longer needed,
+    // we proceed to the following
+    p += verticesNum * 4 + 1
+
+    // Increase the number of vertices
+    vertCount += (verticesNum - 3) * 3 + 3
+  }
+
+  return vertCount
+}
+
+/**
+ * Returns face vertices of the mesh
+ * @todo Make faster
+ */
+export const getFaceVertices = (trianglesBuffer: Int16Array, vertices: Float32Array, texture: ImageData) => {
+  // Number of vertices for generating buffer
+  const vertNumber = countVertices(trianglesBuffer)
+
+  // List of vertices data: origin and uv position on texture
+  const geometryVertices: number[][] = []
+
+  // Current position in buffer
+  let trisPos = 0
+
+  // Processing triangle series
+  while (trianglesBuffer[trisPos]) {
+    // Detecting triangle series type
+    const trianglesType = trianglesBuffer[trisPos] < 0 ? constants.TRIANGLE_FAN : constants.TRIANGLE_STRIP
+
+    // Starting vertex for triangle fan
     let startVert = null
-    let stripCount = 0
-    let len = 0
 
     // Number of following triangles
-    const trisNum = Math.abs(trisBuffer[i])
+    const trianglesNum = Math.abs(trianglesBuffer[trisPos])
 
     // This index is no longer needed,
     // we proceed to the following
-    i++
+    trisPos++
 
-    for (let j = trisNum; j > 0; j--, i += 4) {
-      const vertIndex: number = trisBuffer[i]
-      const vert: number = trisBuffer[i] * 3
-      const light: number = trisBuffer[i + 1] // ?
+    // For counting we will make steps for 4 array items:
+    // 0 — vertex
+    // 1 — light (?)
+    // 2 — fist uv coordinate
+    // 3 — second uv coordinate
+    for (let j = 0; j < trianglesNum; j++, trisPos += 4) {
+      // const vertIndex: number = trianglesBuffer[trisPos]
+      const vert: number = trianglesBuffer[trisPos] * 3
+      // const light: number = trianglesBuffer[trisPos + 1] // ?
 
-      // Vertex coordinate
-      const vertOrigin = new THREE.Vector3(vertices[vert + 0], vertices[vert + 1], vertices[vert + 2])
+      // Vertex data
+      const vertexData = [
+        // Origin
+        vertices[vert + 0],
+        vertices[vert + 1],
+        vertices[vert + 2],
 
-      // Vertex x and y coordinates on texture (from 0.0 to 1.0)
-      const uv = new THREE.Vector2(trisBuffer[i + 2] / texture.width, 1 - trisBuffer[i + 3] / texture.height)
-
-      const vertProps: VertexDesc = {
-        index:  vertIndex,
-        origin: vertOrigin,
-        light,
-        uv
-      }
+        // UV data
+        trianglesBuffer[trisPos + 2] / texture.width,
+        1 - trianglesBuffer[trisPos + 3] / texture.height
+      ]
 
       // Triangle strip. Draw the associated group of triangles.
       // Each next vertex, beginning with the third, forms a triangle with the last and the penultimate vertex.
@@ -146,28 +178,21 @@ export const getFaceVertices = (trisBuffer: Int16Array, vertices: Float32Array, 
       //     ╱    ╲    ╱    ╲    ╱    ╲
       //   ╱________╲╱________╲╱________╲
       // 1          3         5          7
-      if (isTriangleStrip) {
-        if (len > 2) {
-          stripCount++
-
-          if (stripCount % 2 === 0) {
+      if (trianglesType === constants.TRIANGLE_STRIP) {
+        if (j > 2) {
+          if (j % 2 === 0) {
             // even
-            faceVertices.push(
-              faceVertices[faceVertices.length - 3], // previously first one
-              faceVertices[faceVertices.length - 1] // last one
+            geometryVertices.push(
+              geometryVertices[geometryVertices.length - 3], // previously first one
+              geometryVertices[geometryVertices.length - 1] // last one
             )
           } else {
             // odd
-            faceVertices.push(
-              faceVertices[faceVertices.length - 1], // last one
-              faceVertices[faceVertices.length - 2] // second to last
+            geometryVertices.push(
+              geometryVertices[geometryVertices.length - 1], // last one
+              geometryVertices[geometryVertices.length - 2] // second to last
             )
           }
-
-          faceVertices.push(vertProps) // new one
-        } else {
-          faceVertices.push(vertProps)
-          len += 1
         }
       }
 
@@ -178,22 +203,36 @@ export const getFaceVertices = (trisBuffer: Int16Array, vertices: Float32Array, 
       //     ╱    ╲  |  ╱    ╲
       //   ╱________╲|╱________╲
       // 2          1            6
-      if (isTriangleFan) {
-        if (!startVert) {
-          startVert = vertProps
-        }
+      if (trianglesType === constants.TRIANGLE_FAN) {
+        startVert = startVert || vertexData
 
-        if (len > 2) {
-          faceVertices.push(startVert, faceVertices[faceVertices.length - 1])
+        if (j > 2) {
+          geometryVertices.push(startVert, geometryVertices[geometryVertices.length - 1])
         }
-
-        faceVertices.push(vertProps)
-        len += 1
       }
+
+      // New one
+      geometryVertices.push(vertexData)
     }
   }
 
-  return faceVertices
+  // Flattening geometry buffer
+  const geometryBuffer = new Float32Array(vertNumber * 3)
+  const uvBuffer = new Float32Array(vertNumber * 2)
+
+  for (let i = 0; i < vertNumber; i++) {
+    geometryBuffer[i * 3 + 0] = geometryVertices[i][0]
+    geometryBuffer[i * 3 + 1] = geometryVertices[i][1]
+    geometryBuffer[i * 3 + 2] = geometryVertices[i][2]
+
+    uvBuffer[i * 2 + 0] = geometryVertices[i][3]
+    uvBuffer[i * 2 + 1] = geometryVertices[i][4]
+  }
+
+  return {
+    geometry: geometryBuffer,
+    uv:       uvBuffer
+  }
 }
 
 /**
@@ -201,10 +240,38 @@ export const getFaceVertices = (trisBuffer: Int16Array, vertices: Float32Array, 
  * @param modelBuffer Source buffer of MDL file
  */
 export const renderModel = (modelBuffer: ArrayBuffer) => {
+  console.time()
   const modelData: ModelData = parseModel(modelBuffer)
+  console.timeEnd()
+
+  const container = new THREE.Group()
+
+  // Skeleton
+  const skeleton: THREE.Skeleton = createSkeleton(modelData.bones)
+  container.add(skeleton.bones[0])
+
+  // Geometry
+
+  // this.geometry = new THREE.BufferGeometry()
+  // this.geometry.addAttribute('position', this.morphPositions[firstSequenceName][0])
+  // this.geometry.addAttribute('uv', new THREE.BufferAttribute(uvMap, 2))
+
+  // // Mesh material
+  // const material = new THREE.MeshBasicMaterial({
+  //   map: texture,
+  //   side: THREE.DoubleSide,
+  //   transparent: true,
+  //   alphaTest: 0.5,
+  //   morphTargets: true
+  // })
+
+  // // Creating the mesh
+  // this.mesh = new THREE.SkinnedMesh(this.geometry, material)
+
   console.log(modelData)
 
-  createSkeleton(modelData.bones)
-
-  return modelData // temp
+  return {
+    modelData,
+    skeleton
+  }
 }
--- tests/modelRenderer.spec.ts
+++ tests/modelRenderer.spec.ts
@@ -2,7 +2,7 @@ import * as fs                  from 'fs'
 import * as path                from 'path'
 import * as png                 from 'fast-png'
 import { parseModel }           from '../lib/modelParser'
-import { buildTexture }         from '../lib/modelRenderer'
+import * as ModelRenderer       from '../lib/modelRenderer'
 import { MockImageData }        from './tools'
 import { toMatchImageSnapshot } from 'jest-image-snapshot'
 
@@ -14,27 +14,53 @@ Object.defineProperty(global, 'ImageData', { value: MockImageData })
 
 const leetPath = path.resolve(__dirname, '../models/leet.mdl')
 const leetBuffer: ArrayBuffer = fs.readFileSync(leetPath).buffer
-const modelData = parseModel(leetBuffer)
+const leetModelData = parseModel(leetBuffer)
 
 describe('test building functions', () => {
   describe('test textures building', () => {
     test('just building texture without errors', () => {
-      const texture = modelData.textures[0]
-      buildTexture(leetBuffer, texture)
+      const texture = leetModelData.textures[0]
+      ModelRenderer.buildTexture(leetBuffer, texture)
     })
 
     test('should build valid skin texture', () => {
-      const texture = modelData.textures[0]
-      const buildedSkin: Uint8Array = png.encode(buildTexture(leetBuffer, texture))
+      const texture = leetModelData.textures[0]
+      const buildedSkin: Uint8Array = png.encode(ModelRenderer.buildTexture(leetBuffer, texture))
 
       expect(Buffer.from(buildedSkin)).toMatchImageSnapshot()
     })
 
     test('should build valid backpack texture', () => {
-      const texture = modelData.textures[2]
-      const buildedSkin: Uint8Array = png.encode(buildTexture(leetBuffer, texture))
+      const texture = leetModelData.textures[2]
+      const buildedSkin: Uint8Array = png.encode(ModelRenderer.buildTexture(leetBuffer, texture))
 
       expect(Buffer.from(buildedSkin)).toMatchImageSnapshot()
+    })
+  })
+
+  describe('test geometry building', () => {
+    const triangles = leetModelData.triangles[0][0][0]
+    const vertices = leetModelData.vertices[0][0]
+    const textureImageData = ModelRenderer.buildTexture(leetBuffer, leetModelData.textures[0])
+
+    test('speed of geometry building', () => {
+      ModelRenderer.getFaceVertices(triangles, vertices, textureImageData)
+    })
+
+    test('should build the proper number of vertices', () => {
+      expect(ModelRenderer.countVertices(leetModelData.triangles[0][0][0])).toBe(2220)
+      expect(ModelRenderer.countVertices(leetModelData.triangles[0][0][1])).toBe(36)
+      expect(ModelRenderer.countVertices(leetModelData.triangles[1][1][0])).toBe(132)
+    })
+
+    test('should build geometry buffer', () => {
+      const { geometry } = ModelRenderer.getFaceVertices(triangles, vertices, textureImageData)
+      expect(geometry).toMatchSnapshot('geometry buffer')
+    })
+
+    test('should building uv map', () => {
+      const { uv } = ModelRenderer.getFaceVertices(triangles, vertices, textureImageData)
+      expect(uv).toMatchSnapshot('uv buffer')
     })
   })
 })
